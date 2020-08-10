2d76f0f9c1f1d906d636c2510625598245680207
Richard Finegan <richiefinegan1990@gmail.com>
2020-08-10 21:41:19 +0100

    Added Google maps API and maps.js

---
+++ assets/js/maps.js
@@ -0,0 +1,9 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: {
+            lat: 53.279995244347994,
+            lng: -6.363830566406251
+        }
+    });
+}
