2dcf7eecaccbf5192c60c97eba31fda6c2cedf18
Richard Finegan <richiefinegan1990@gmail.com>
2020-08-11 19:51:13 +0100

    Added markers to map

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,9 +1,29 @@
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 4,
+        zoom: 6,
         center: {
             lat: 53.279995244347994,
             lng: -6.363830566406251
         }
+    });
+
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    var locations = [
+        {lat: 53.279995244347994, lng: -6.363830566406251 },
+        {lat: 53.27342595846812, lng: -9.047412872314455 },
+        {lat: 51.887511191058756, lng: -8.46676826477051 },
+        {lat: 54.58434909323848, lng: -5.931200981140137 },
+    ];
+
+    var markers = locations.map(function(location, i){
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]            
+        });
+    });
+
+    var markerCluster = new MarkerClusterer(map, markers, {
+        imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
     });
 }
