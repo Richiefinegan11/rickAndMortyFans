526fdfecdddbc2f44e7da0e3f24876fd7a0affab
Richard Finegan <richiefinegan1990@gmail.com>
2020-08-14 14:48:50 +0100

    Added style to map section

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,14 +1,16 @@
-function sendMail(contactForm) {
-    emailjs.sned("gmail", "rickAndMortyFanClub", {
-        "from_name": contactForm.name,value,
+  function sendMail(contactForm) {
+    emailjs.send("gmail", "rickandmortyfanclub", {
+        "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
         "join_the_club": contactForm.jointheclub.value
     })
     .then(
         function(response) {
-            console.log ("SUCCESS", response);
+            console.log("SUCCESS", response);
         },
         function(error) {
             console.log("FAILED", error);
-        });
+        }
+    );
+    return false;  // To block from loading a new page
 }
